b158d2c621d641da9a09e53d1c6bcb32311a46d5
Lakshya Pandey <lakshyapandey242@gmail.com>
2026-02-23 18:16:02 -0500

    Delete kill_port.bat

--- kill_port.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Script to kill process using port 8000
-echo Finding process using port 8000...
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :8000 ^| findstr LISTENING') do (
-    echo Killing process ID: %%a
-    taskkill /F /PID %%a
-)
-echo Done!
-pause
